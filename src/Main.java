4a7530fb54b81aa222f24d174466d75174914f80
Peter <pli1204@students.cs.ubc.ca>
2023-01-17 21:41:49 -0800

    Moved text grabbing into try catch

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ import org.jsoup.select.Elements;
 
 public class Main {
 
-    static Document doc;
-    static Elements content;
+    public static Document doc;
+    public static Elements content;
+    public static String text;
 
     public static void main(String[] args) {
         String url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=PHYS&course=131&section=20W";
@@ -14,12 +15,12 @@ public class Main {
         try {
             doc = Jsoup.connect(url).get();
 
-            content = doc.select("table");
+            content = doc.select("table.'table");
+
+            String text = content.text();
         } catch (Exception e) {
             System.out.println("An error occurred");
         }
-
-        String text = content.text();
 
         System.out.println(text);
     }
